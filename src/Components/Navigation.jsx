4a9143905a59d7fece1eefb7f5eeb988a7796dd7
Neve-06 <6255234@rts-365.dk>
2025-08-13 12:53:59 +0200

    added logo and changed link names

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,13 +1,15 @@
 import { NavLink } from "react-router"
 import './navigation.scss'
-
 export default function Navigation() {
 
     return (
         <nav className="navigation">
+            <figure className="logo">
+            <img src="src/img/logo.svg" alt="logo" />
+            </figure>
                 <NavLink to="/">Home</NavLink>
-                <NavLink to="/list">List</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
+                <NavLink to="/list">Boliger til salg</NavLink>
+                <NavLink to="/contact">Kontakt os</NavLink>
             
             </nav>
     )
