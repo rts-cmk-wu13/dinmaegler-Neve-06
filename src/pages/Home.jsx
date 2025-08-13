a84ea4dffb7bb943de3e4732b2ba7f15e5d24656
Neve-06 <6255234@rts-365.dk>
2025-08-13 10:12:34 +0200

    added components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Link, NavLink } from "react-router"
-
+import Hero from "../Components/Hero"
+import Family from "../Components/Family"
+import Boliger from "../Components/Boliger"
 
 export default function Home() {
 
     return (
         <>
-            
-            <h1>Home</h1>
-            <Link to="/about">go to about!</Link>
+            <Hero />
+            <Family/>
+            <Boliger/>
         </>
     )
 }
