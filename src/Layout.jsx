7861cf37d73f93b204ef5273cbb6616465b7d94e
Neve-06 <6255234@rts-365.dk>
2025-08-14 13:07:06 +0200

    replaced footer tag with footer component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 
 import { Outlet } from 'react-router'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 import "./styles/reset.scss"
 import "./styles/responsivness.scss"
 import "./styles/logo.scss"
 import "./styles/family.scss"
 import "./styles/homeapartments.scss"
+import "./styles/footer.scss"
 
 function App() {
  
@@ -16,8 +18,7 @@ function App() {
       <main>
         <Outlet />
       </main>
-      <footer>
-      </footer>
+      <Footer/>
     </>
   )
 }
