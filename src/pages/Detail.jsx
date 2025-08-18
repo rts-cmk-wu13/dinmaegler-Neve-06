c249fbbc124bae43c1d65710f7698f75f5e30287
Neve-06 <6255234@rts-365.dk>
2025-08-18 12:36:08 +0200

    added content inside detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,34 @@
     import { useLoaderData } from "react-router"
 
 export default function Detail() {
-    const user = useLoaderData(); // indlæs data fra loader funktion
+    const home = useLoaderData(); // indlæs data fra loader funktion
 
     return  (
         <>
-        <p>user details</p>
-        <h2>{user.name}</h2>
+        <img className="full-width" src={home.images[0]?.formats?.thumbnail?.url} alt={home.name} />
+        <section>
+            <h2>{home.adress1}</h2>
+            <section>
+            <p>{home.city}</p>
+            <p>{home.postalcode}</p>
+            </section>
+        </section>
+        <div>
+        <section  className="description__container">
+            <h2>Beskrivelse</h2>
+            <p>{home.description}</p>
+        </section>
+        <section  className="description__container">
+            <h2>Ansvalig Mælger</h2>
+            <figure>
+            <img className="full-width" src={home.agent.image?.formats?.thumbnail?.url} alt={home.agent.name} />
+            </figure>
+            <h2>{home.agent.name}</h2>
+            <p>{home.agent.title}</p>
+            <p>{home.agent.phone}</p>
+            <p>{home.agent.email}</p>
+        </section>
+        </div>
         </>
 
     )
