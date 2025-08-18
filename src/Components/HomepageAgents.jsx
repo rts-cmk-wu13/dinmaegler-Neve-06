b8e133aed3a8c7bc038f31476d574cce8196cc73
Neve-06 <6255234@rts-365.dk>
2025-08-18 12:35:53 +0200

    Added homepageagents component

---
+++ src/Components/HomepageAgents.jsx
@@ -0,0 +1,32 @@
+import { MdMail } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
+import { useLoaderData } from "react-router";
+
+export default function HomepageAgents() {
+
+  const {agents} = useLoaderData()
+
+  return (
+    <>
+      <section className="no-columns">
+        <h2>
+          Mød vores engagerede medarbejdere
+        </h2>
+        <p>Din Mægler er garant for altid veluddannet assistance i dit boligsalg. Kontakt en af vores medarbejdere.</p>
+      </section>
+      <section>
+        {agents.map((agent) => (
+          <div className="agent__container" key={agent.id}>
+            <figure className="agent__figure">
+              <img src={agent.image?.formats?.thumbnail?.url} alt={agent.name} />
+            </figure>
+            <h2>{agent.name}</h2>
+            <p>{agent.title}</p>
+            <MdMail />
+            <FaLinkedinIn />
+          </div>
+        ))}
+      </section>
+    </>
+  )
+}
