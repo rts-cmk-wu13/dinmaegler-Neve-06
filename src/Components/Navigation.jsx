6bf0b8c144ac87e1270b250dc6b43a35033ed4ac
Neve-06 <6255234@rts-365.dk>
2025-08-18 11:11:46 +0200

    added navlink around the logo

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,10 +4,11 @@ export default function Navigation() {
 
     return (
         <nav className="navigation">
-            <figure className="logo">
-            <img src="src/img/logo.svg" alt="logo" />
-            </figure>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/">
+                <figure className="logo">
+                <img src="src/img/logo.svg" alt="logo" />
+                </figure>
+                </NavLink>
                 <NavLink to="/list">Boliger til salg</NavLink>
                 <NavLink to="/contact">Kontakt os</NavLink>
             
