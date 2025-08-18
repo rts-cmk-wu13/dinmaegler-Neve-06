7009326c7ac45ac9306c6bfe7ee99d9228e9bb6b
Neve-06 <6255234@rts-365.dk>
2025-08-18 12:37:26 +0200

    added content on list cards

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -12,10 +12,12 @@ export default function List() {
             {homes.map(home => (
                 <li className="home__li" key={home.id}>
                     <Link to={`/list/${home.id}`}>
-                    {/* <p>{home.adress1}</p> */}
                     <figure className="home__figure">
                     <img src={home.images[0]?.formats?.thumbnail?.url} alt={home.name} />
                     </figure>
+                    <p>{home.adress1}</p>
+                    <p>{home.postalcode}</p>
+                    <p>{home.city}</p>
                     </Link>
                 </li>
             ))}
