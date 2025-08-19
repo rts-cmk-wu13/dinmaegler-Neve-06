a9195e07b9bd37c2a19bcdb69afb6e856d544539
Neve-06 <6255234@rts-365.dk>
2025-08-19 10:34:11 +0200

    added the logo to the footer ( Its comically large )

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,34 +1,41 @@
-import { FaPhoneAlt, FaPaperPlane  } from "react-icons/fa";
+import { FaPhoneAlt, FaPaperPlane } from "react-icons/fa";
 import { FaLocationPin } from "react-icons/fa6";
 import { Link } from "react-router";
+import logo from '../img/logo.svg';
 export default function Footer() {
 
     return (
         <>
-        <footer >
-            <section> 
-            <section className="footer__section">
-                <FaPhoneAlt/>
-                <p>Ring til os</p>
-                <p>+45 7070 4000</p>
-                <FaPaperPlane/>
-                <p>Send en mail</p>
-                <p>4000@dinmaegler.com</p>
-                <FaLocationPin/>
-                <p>Butik</p>
-                <p>Stændertorvet 78, 4000 Roskilde</p>
-            </section>
-            <section className="footer__section">
-                <h2>Quick Links</h2>
-                <Link to="list">Boliger til salg</Link>
-                <Link to="agents">Mæglere</Link>
-                <Link to="contact">Kontakt os</Link>
-            </section>
-            </section>
-            <section className="footer__bottom full-width">
-                <p>Layout By Jit Banik 2020</p>
-            </section>
-        </footer>
+            <footer >
+                <section>
+                    <section className="no-columns">
+                        <figure className="logo__container">
+                        <img src={logo} alt="logo" />
+                        </figure>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
+                    </section>
+                    <section className="footer__section">
+                        <FaPhoneAlt />
+                        <p>Ring til os</p>
+                        <p>+45 7070 4000</p>
+                        <FaPaperPlane />
+                        <p>Send en mail</p>
+                        <p>4000@dinmaegler.com</p>
+                        <FaLocationPin />
+                        <p>Butik</p>
+                        <p>Stændertorvet 78, 4000 Roskilde</p>
+                    </section>
+                    <section className="footer__section">
+                        <h2>Quick Links</h2>
+                        <Link to="list">Boliger til salg</Link>
+                        <Link to="agents">Mæglere</Link>
+                        <Link to="contact">Kontakt os</Link>
+                    </section>
+                </section>
+                <section className="footer__bottom full-width">
+                    <p>Layout By Jit Banik 2020</p>
+                </section>
+            </footer>
         </>
     )
 }
