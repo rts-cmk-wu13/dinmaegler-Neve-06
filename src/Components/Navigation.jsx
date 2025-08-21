bd902a4056245c9605b9b51a1006ceed96fcc8ee
Neve-06 <6255234@rts-365.dk>
2025-08-21 09:23:46 +0200

    added agents to Nav

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -10,6 +10,7 @@ export default function Navigation() {
                 <img src={logo} alt="logo" />
                 </figure>
                 </NavLink>
+                <NavLink to="/agents">Mælgere</NavLink>
                 <NavLink to="/list">Boliger til salg</NavLink>
                 <NavLink to="/contact">Kontakt os</NavLink>
             
