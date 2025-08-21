4c4f961beee42b6ae1396482c4a1e22de0deb517
Neve-06 <6255234@rts-365.dk>
2025-08-21 13:46:36 +0200

    added content inside agentlist and agent detail page

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -1,6 +1,30 @@
+import { useLoaderData } from "react-router"
+import Contact from "./Contact"
+
 export default function Agent() {
 
+    const agent = useLoaderData()
     return (
-        <></>
+        <>
+        <article className="agent__container">
+            <figure className="agent__figure-detail">
+            <img src={agent.image?.formats?.thumbnail.url} alt={agent.name} />
+            </figure>
+            <section className="agent__section">
+                <h2>{agent.name}</h2>
+                <p>{agent.title}</p>
+                <p>{agent.phone}</p>
+                <p>{agent.email}</p>
+            </section>
+        <section className="agent__description">
+            <h2>Om {agent.name}</h2>
+            <p>{agent.description}</p>
+        </section>
+        </article>
+        <section className="agent__contact">
+            <p className="agent__contact-text">Kontakt {agent.name}</p>
+        {/* <Contact/> */}
+        </section>
+        </>
     )
 }
--- src/pages/Agents.jsx
+++ src/pages/Agents.jsx
@@ -1,6 +1,25 @@
+import { Link, useLoaderData } from "react-router";
+import "../api/agents.js"
+
 export default function Agents() {
+    const agents = useLoaderData();
 
     return (
-        <></>
+        <>
+             <ul>
+                    {agents.map(agent => (
+                        <li className="agent__li" key={agent.id}>
+                    <Link to={`/agents/${agent.id}`}>
+                    <figure className="agent__figure">
+                    <img src={agent.image?.formats?.thumbnail?.url} alt={agent.name} />
+                    </figure>
+                    <p>{agent.name}</p>
+                    <p>{agent.title}</p>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        
+        </>
     )
 }
