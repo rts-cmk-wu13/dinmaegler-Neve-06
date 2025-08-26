68bb5172ea3031ad755cb7e433a3076764882c32
Neve-06 <6255234@rts-365.dk>
2025-08-26 12:48:10 +0200

    added classes, styling and changes to contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,24 +6,28 @@ export default function Contact() {
 
     return (
     <>
-        <h1>Contact</h1>
-        <Form method="post">
+        <Form method="post" className="contact__form">
             <div className="formgroup">
-                <label className="formgroup__label" htmlFor="name">Name:</label>
-                <input type="text" name="name" placeholder="Name" />
+                <label className="formgroup__label" htmlFor="name">Navn</label>
+                <input type="text" name="name" placeholder="Indtast navn" />
                 <p className="error">{errors && errors?.name?.errors[0]}</p>
             </div>
             <div className="formgroup">
-                <label htmlFor="email">Email:</label>
-                <input type="email" name="email" placeholder="Email" />
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" placeholder="Indtast email" />
                 <p className="error">{errors && errors?.email?.errors[0]}</p>
             </div>
             <div className="formgroup">
-                <label htmlFor="message">Message:</label>
-                <textarea name="message" placeholder="Message"></textarea>
+                <label htmlFor="message">Emne</label>
+                <input type="text" name="subject" placeholder="Hvad drejer din henvendelse sig om?" />
+                <p className="error">{errors && errors?.subject?.errors[0]}</p>
+            </div>
+            <div className="formgroup">
+                <label htmlFor="message">Besked</label>
+                <textarea className="textarea__size" maxLength={50} name="message" placeholder="Skriv din besked her..."></textarea>
                 <p className="error">{errors && errors?.message?.errors[0]}</p>
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit" className="home__button">Send Besked</button>
         </Form>
         </>
     )
