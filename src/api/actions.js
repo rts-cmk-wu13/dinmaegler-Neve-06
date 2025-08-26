a1bb414f3198f6be2b26a0e11d2a0b8a4bb8299d
Neve-06 <6255234@rts-365.dk>
2025-08-26 12:54:27 +0200

    removed unnecessary fetch

--- src/api/actions.js
+++ src/api/actions.js
@@ -22,19 +22,6 @@ export async function handleSubmit ({ request }) {
             console.log(errors)
             return errors.properties
         } 
-        
-
-            const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(result.data) 
-            })
-          
-            if(!response.ok) {
-                throw new Error("Could not save data")
-            }
 
             console.log("data was sent!!!")
             redirect("/")
