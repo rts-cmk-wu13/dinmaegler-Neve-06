d5c703976085324dc980af7d859354899092eba7
Neve-06 <6255234@rts-365.dk>
2025-08-29 12:26:50 +0200

    added contact content

--- src/pages/Contactinfo.jsx
+++ src/pages/Contactinfo.jsx
@@ -1,9 +1,28 @@
-
+import { FaPhoneAlt, FaPaperPlane } from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
 
 export default function Contactinfo() {
 
     return (
         <>
+        <section className="contactinfo__container">
+        <section className="contactinfo__section">
+            <FaPhoneAlt/>
+            <h3>Ring til os</h3>
+            <p>++45 7070 4000</p>
+        </section>
+        <section className="contactinfo__section">
+            <FaPaperPlane/>
+            <h3>Send os en besked</h3>
+            <p>4000@dinmaegler.dk</p>
+        </section>
+        <section className="contactinfo__section">
+            <FaLocationPin/>
+            <h3>Besøg butikken</h3>
+            <p>Stændertorvet 78,</p>
+            <p>4000 Roskilde</p>
+        </section>
+        </section>
         </>
     )
 }
--- src/pages/Contactpage.jsx
+++ src/pages/Contactpage.jsx
@@ -1,4 +1,5 @@
 import Contact from "./Contact";
+import Contactinfo from "./Contactinfo";
 import banner from "../img/banner.png"
 
 export default function Contactpage() {
@@ -6,7 +7,12 @@ export default function Contactpage() {
     return (
         <>
         <img className="full-width" src={banner} alt="banner" />
+        <div className="contact__form">
+        <h2 className="contact__form">Vi sidder klar til at besvare dine spørgsmål</h2>
+        <p className="contact__form">Der kan opstå tvivl om mange ting nå man gerne vil, eller er i gang med at sælge sin bolig. Vores medarbejdere sider klar alle ugens dage til at svare på dine spørgsmål.</p>
+        </div>
         <Contact />
+        <Contactinfo />
         </>
     )
 }
