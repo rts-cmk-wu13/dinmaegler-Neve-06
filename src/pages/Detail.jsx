0af6ac2bdc1d02ae8139d40524f39e3fc070f7e4
Neve-06 <6255234@rts-365.dk>
2025-09-02 10:29:45 +0200

    added more content in details

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,26 +1,57 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react"
 import { useLoaderData } from "react-router"
 import { AiTwotonePicture } from "react-icons/ai";
-import { FaLocationDot, FaHeart  } from "react-icons/fa6";
+import { FaLocationDot, FaHeart } from "react-icons/fa6";
 import { IoLayersOutline } from "react-icons/io5";
+import { useFavorites } from "../contexts/FavoritesContext"; // ✅ import
 
 
 export default function Detail() {
     const home = useLoaderData(); // indlæs data fra loader funktion
 
+    const { id } = useParams();
+    const [getHome, setHome] = useState(null);
+    const [error, setError] = useState(null);
+    const { toggleFavorite, favorites } = useFavorites();
+    useEffect(() => {
+        fetch(`https://dinmaegler.onrender.com/homes/${id}`)
+            .then((res) => {
+                if (!res.ok) throw new Error("Network response not ok");
+                return res.json();
+            })
+            .then((data) => setHome(data))
+            .catch((err) => setError("Could not fetch home details."));
+    }, [id]);
+
+    if (error) return <p className="text-red-500">{error}</p>;
+    if (!home) return <p>Loading...</p>;
+
+
+
+
+    const isFavorited = favorites.includes(home.id);
     return (
         <>
             <img className="full-width" src={home.images[0]?.formats?.thumbnail?.url} alt={home.name} />
             <section className="home__info-top">
                 <section>
-                <h2>{home.adress1}</h2>
+                    <h2>{home.adress1}</h2>
                     <p>{home.city}</p>
                     <p>{home.postalcode}</p>
                 </section>
                 <section>
-                    <AiTwotonePicture size={45}/>
-                    <IoLayersOutline size={45}/>
-                    <FaLocationDot size={45}/>
-                    <FaHeart size={45}/>
+                    <AiTwotonePicture size={45} />
+                    <IoLayersOutline size={45} />
+                    <FaLocationDot size={45} />
+                <button
+  onClick={() => toggleFavorite(home)}
+  className="col-start-8 size-12 "
+>
+  <FaHeart
+    className={`size-12 col-start-8 ${isFavorited ? "text-red-500" : "text-gray-400"}`}
+  />
+</button>
                 </section>
                 <section>
                     <p>Kr. {home.price}</p>
@@ -38,7 +69,7 @@ export default function Detail() {
                     <p>Kælder:{home.basementsize}</p>
                     <p>Byggeår:{home.built}</p>
                     <p>Ombygget:{home.remodel}</p>
-                    <p>Energimærke:{home.energylabel}</p>   
+                    <p>Energimærke:{home.energylabel}</p>
                 </div>
                 <div className="home__info">
 
