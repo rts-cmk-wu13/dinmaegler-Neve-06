57849b79bb804a384b4337a27a0801f47f6c43ed
Neve-06 <6255234@rts-365.dk>
2025-09-02 14:05:24 +0200

    added more changes to favorites: Moved Link inside the figure tag, added classes to a div containing info. also added button to remove favorites ( not working yet )

--- src/Components/Favorite.jsx
+++ src/Components/Favorite.jsx
@@ -14,15 +14,18 @@ export default function Favorite() {
       <img className="full-width" src={banner} alt="banner" />
       <ul>
         {favorites.map(home => (
-          <li className="home__li" key={home.id}>
+          <li className="favorites__li" key={home.id}>
+              <figure className="favorites__figure">
             <Link to={`/list/${home.id}`}>
-              <figure className="home__figure">
                 <img src={home.images[0]?.formats?.thumbnail?.url} alt={home.name} />
-              </figure>
-              <p>{home.adress1}</p>
-              <p>{home.postalcode}</p>
-              <p>{home.city}</p>
             </Link>
+              </figure>
+              <div className="favorites__info" >
+              <p>{home.adress1}</p>
+              <p>{home.postalcode} {home.city}</p>
+              <p>{home.type} Ejerudgift: {home.netto}</p>
+              </div>
+              <button className="home__button favorites__button">Fjern Fra Favoritter</button>
           </li>
         ))}
       </ul>
